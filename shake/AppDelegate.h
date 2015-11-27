0424e0032f7b6113412b94877c426f574fd73c89
bigheart <liujian@lantugroup.cn>
2015-11-27 09:50:54 +0800

    Initial Commit

---
+++ shake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shake
+//
+//  Created by lj on 15/11/27.
+//  Copyright © 2015年 lj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shake
+//
+//  Created by lj on 15/11/27.
+//  Copyright © 2015年 lj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
